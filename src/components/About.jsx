900139a2f990502caf4549cf5ff00fe0609adf81
TeeAkinlade <taiye.akinlade!@gmail.com>
2024-11-02 01:42:34 +0100

    changes to about component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
             <div className="p-4 mx-auto flex flex-col justify-center w-full h-full">
                 <div className="pb-8">
                     <p className='text-4xl font-bold inline border-b-4 border-black'>About</p>
-                    <p className=" text-lg md:text-xl font-medium mt-20 px-6 md:px-12 leading-10 lg:px-16">
+                    <p className=" text-lg md:text-xl font-medium mt-20 px-6 md:px-12 leading-7 lg:px-16">
                       My name is Taiye Akinlade, a passionate Frontend Developer with a keen eye for creating intuitive, visually appealing, and user-friendly digital experiences. I specialize in translating design concepts into responsive, efficient, and scalable code, ensuring that each project not only looks great but performs seamlessly. With expertise in HTML, CSS, JavaScript, and modern frameworks like React, I continuously strive to stay at the forefront of web development trends and best practices. I believe in the power of clean, accessible code and a user-first approach to build meaningful digital solutions that make a difference.
 
                       <p className='pt-3'>Let’s bring ideas to life, pixel by pixel!</p>
